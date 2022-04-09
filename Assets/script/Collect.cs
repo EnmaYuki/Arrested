ffaf8eca4667d91587959d4376015e8043382a86
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-04-09 12:12:36 +0800

    update

---
+++ Assets/script/Collect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+	
+	public AudioClip CoinSound;
+	
+	AudioSource AS;
+
+	void Start () {
+		AS = GetComponent<AudioSource>();
+	}
+	
+	void OnTriggerEnter (Collider col) {
+		if (col.CompareTag("Player")) {
+			//CoinCount.UpdateCoin(1);
+			AS.PlayOneShot(CoinSound);
+			Destroy(gameObject);
+			StartCoroutine(SpeedUp());
+		}
+	}
+	private IEnumerator SpeedUp()
+	{
+		CameraController.moveSpeed1 = 100;
+		yield return new WaitForSeconds(5.0f);
+		CameraController.moveSpeed1 = 20;
+	}
+}
---
+++ Assets/script/PlayerLife.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLife : MonoBehaviour
+{
+
+	PlayerController control;
+	CapsuleCollider capcol;
+	Animator anim;
+	AudioSource AS;
+	public static bool dead;
+	public GameObject deadCam;
+
+	public AudioClip dieSound;
+
+	public GameObject ResultMenu;
+
+	void Start()
+	{
+		dead = false;
+		control = GetComponent<PlayerController>();
+		capcol = GetComponent<CapsuleCollider>();
+		anim = GetComponent<Animator>();
+		AS = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		if (transform.position.y < -0.5f && !dead)
+		{
+			capcol.enabled = false;
+			dead = true;
+			AS.PlayOneShot(dieSound);
+		}
+
+		if (dead)
+		{
+			control.enabled = false;
+			deadCam.transform.parent = null;
+			ResultMenu.SetActive(true);
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.CompareTag("Obstacle"))
+		{
+			dead = true;
+			anim.SetTrigger("die");
+			AS.PlayOneShot(dieSound);
+			transform.parent = null;
+		}
+		else if (col.gameObject.CompareTag("Enemy"))
+		{
+			dead = true;
+			transform.parent = null;
+			//win
+			//WinMenu.SetActive(true);
+			control.enabled = false;
+			deadCam.transform.parent = null;
+		}
+	}
+}
